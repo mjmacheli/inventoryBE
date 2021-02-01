53ed173b7f7eb085e20d02a9f406eaf03eb52c18
MJ Macheli <machelimail@gmail.com>
2021-02-02 00:45:56 +0200

    * InventoryApp.sln: initial commit with user endpoint and swagger
    * Program.cs:
    * Startup.cs:
    * User.cs:
    * appsettings.json:
    * WeatherForecast.cs:
    * InventoryApp.csproj:
    * project.nuget.cache:
    * project.assets.json:
    * UserService.cs:
    * appsettings.Development.json:
    * UserController.cs:
    * DnsClient.dll:
    * launchSettings.json:
    * InventoryApp.pdb:
    * MongoDB.Bson.dll:
    * InventoryApp.dll:
    * InventoryApp.pdb:
    * InventoryApp.dll:
    * SharpCompress.dll:
    * InventoryAppDataSettings.cs:
    * MongoDB.Driver.dll:
    * Newtonsoft.Json.dll:
    * InventoryApp.dll:
    * InventoryApp.csproj.nuget.g.props:
    * InventoryApp.dll:
    * Microsoft.OpenApi.dll:
    * InventoryApp.csproj.nuget.g.targets:
    * InventoryApp.deps.json:
    * MongoDB.Driver.Core.dll:
    * WeatherForecastController.cs:
    * InventoryApp.csproj.nuget.dgspec.json:
    * Newtonsoft.Json.Bson.dll:
    * InventoryApp.assets.cache:
    * MongoDB.Libmongocrypt.dll:
    * appsettings.Development.json:
    * InventoryApp.AssemblyInfo.cs:
    * InventoryApp.runtimeconfig.json:
    * libzstd.dll:
    * InventoryApp.csproj.CopyComplete:
    * snappy32.dll:
    * snappy64.dll:
    * Microsoft.IdentityModel.Tokens.dll:
    * Swashbuckle.AspNetCore.Swagger.dll:
    * Microsoft.AspNetCore.JsonPatch.dll:
    * InventoryApp.runtimeconfig.dev.json:
    * InventoryApp.genruntimeconfig.cache:
    * System.IdentityModel.Tokens.Jwt.dll:
    * Microsoft.IdentityModel.Logging.dll:
    * Swashbuckle.AspNetCore.SwaggerUI.dll:
    * InventoryApp.AssemblyInfoInputs.cache:
    * Swashbuckle.AspNetCore.SwaggerGen.dll:
    * Microsoft.IdentityModel.Protocols.dll:
    * InventoryApp.csproj.FileListAbsolute.txt:
    * Microsoft.IdentityModel.JsonWebTokens.dll:
    * InventoryApp.RazorTargetAssemblyInfo.cache:
    * InventoryApp.csproj.CoreCompileInputs.cache:
    * Microsoft.AspNetCore.Mvc.NewtonsoftJson.dll:
    * .NETCoreApp,Version=v5.0.AssemblyAttributes.cs:
    * InventoryApp.MvcApplicationPartsAssemblyInfo.cs:
    * InventoryApp.StaticWebAssets.xml:
    * Microsoft.AspNetCore.Authentication.JwtBearer.dll:
    * InventoryApp.MvcApplicationPartsAssemblyInfo.cache:
    * Microsoft.IdentityModel.Protocols.OpenIdConnect.dll:
    * Microsoft.AspNetCore.Authentication.OpenIdConnect.dll:
    * InventoryApp.GeneratedMSBuildEditorConfig.editorconfig:
    * InventoryApp.StaticWebAssets.Manifest.cache:
    
    * appsettings.json: initial commit

---
+++ InventoryApp/Models/InventoryAppDataSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace InventoryApp.Models
+{
+    public class InventoryAppDataSettings : IinventoryAppDatabaseSettings
+    {
+        public string InventoryAppCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IinventoryAppDatabaseSettings
+    {
+        string InventoryAppCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ InventoryApp/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace InventoryApp.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string names { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string dob { get; set; }
+
+        public string country { get; set; }
+
+        public string province { get; set; }
+
+        public string city { get; set; }
+
+        public string area { get; set; }
+
+        public string street { get; set; }
+
+    }
+}
