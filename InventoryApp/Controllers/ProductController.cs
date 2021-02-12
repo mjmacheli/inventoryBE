9676157c27b4113622349f5fcf81d87f34ce0d62
kyle <manganyikyle@gmail.com>
2021-02-12 18:10:25 +0200

    fix

--- InventoryApp/Controllers/ProductController.cs
+++ InventoryApp/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
 using InventoryApp.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic:
+using System.Collections.Generic;
 
 namespace InventoryApp.Controllers
 {
