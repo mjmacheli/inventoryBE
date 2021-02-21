16cd5f0caf4e83f0c02ab418d2315c452a4be8f0
MJ Macheli <machelimail@gmail.com>
2021-02-22 01:49:44 +0200

    total sales

--- InventoryApp/Controllers/StoreController.cs
+++ InventoryApp/Controllers/StoreController.cs
@@ -76,7 +76,7 @@ namespace InventoryApp.Controllers
         [HttpGet("total-sales/{storeId}")]
         public IActionResult totalSales(string storeId)
         {
-            return Ok(dBContext.sales.Where(x => x.id == storeId));
+            return Ok(dBContext.sales.Where(x => x.storeID == storeId));
         }
     }
 
